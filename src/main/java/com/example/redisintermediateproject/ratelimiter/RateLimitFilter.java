1e8c047e708b57b94aadbcbef4e591fd62e5de75
leechangtaek <ckdxor1014@naver.com>
2026-04-16 22:02:45 +0900

    fix:악의적인 요청 방지하기 위해 제안

---
+++ src/main/java/com/example/redisintermediateproject/ratelimiter/RateLimitFilter.java
@@ -0,0 +1,40 @@
+package com.example.redisintermediateproject.ratelimiter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class RateLimitFilter extends OncePerRequestFilter {
+    private final RateLimiter rateLimiter;
+
+    public RateLimitFilter(RateLimiter rateLimiter) {
+        this.rateLimiter = rateLimiter;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String userId = request.getHeader("USER-ID");
+
+        if(userId == null){
+            return;
+        }
+
+        if(!rateLimiter.allow(userId)){
+            response.setStatus(429);
+            return;
+        }
+        filterChain.doFilter(request, response);
+
+    }
+}
--- src/main/java/com/example/redisintermediateproject/ratelimiter/RateLimiter.java
+++ src/main/java/com/example/redisintermediateproject/ratelimiter/RateLimiter.java
@@ -1,5 +1,49 @@
 package com.example.redisintermediateproject.ratelimiter;
 
-public class RateLimiter {
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RateLimiter {
+    private static final long RATE_LIMIT_TIME_MS = 10_000;
+    private static final long MAX_REQUESTS = 5;
+
+
+    private final RedisTemplate<String,String> redisTemplate;
+
+
+    public RateLimiter(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public boolean allow(String userId){
+        long now  = System.currentTimeMillis();
+
+        String redisKey = "rate_limit:"+userId;
+
+        String requestId = UUID.randomUUID().toString();
+
+        redisTemplate
+                .opsForZSet()
+                .removeRangeByScore(redisKey,0,now - RATE_LIMIT_TIME_MS);
+
+        redisTemplate
+                .opsForZSet()
+                .add(redisKey,requestId,now);
+
+        Long count = redisTemplate
+                .opsForZSet()
+                .size(redisKey);
+
+        redisTemplate.expire(
+                redisKey,
+                10_000,
+                TimeUnit.MILLISECONDS
+        );
+
+        return count<= MAX_REQUESTS;
+    }
 }
